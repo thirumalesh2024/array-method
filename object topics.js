5a66b58ef084a19bf8afbc43de3aa6e21756de3b
thirumalesh2024 <halavaththirumalesh73143@gmail.com>
2025-01-06 14:51:40 +0530

    object topics

---
+++ object topics.js
@@ -0,0 +1,5 @@
+index.js:7 Before deletion: Objectcity: "hyderabad"street: "123 Main St"[[Prototype]]: Object
+index.js:12 After deletion: Objectcity: "hyderabad"street: "123 Main St"[[Prototype]]: Object
+index.js:23 Objectage: 22email: "halavaththirumalesh73143@gmail.com"name: "Halavath Thirumalesh"phone: "555-9876"[[Prototype]]: Object
+index.js:33 hyderabad
+index.html:43 Live reload enabled.
